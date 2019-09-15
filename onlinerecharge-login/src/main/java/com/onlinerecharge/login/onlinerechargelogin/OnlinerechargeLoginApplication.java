e42363f43b3c27e9f1583435a1f319eb2eabf88a
sayyad123 <sayyadsubahan313@yahoo.com>
2019-09-15 20:42:57 +0530

    New jar Project

---
+++ onlinerecharge-login/src/main/java/com/onlinerecharge/login/onlinerechargelogin/OnlinerechargeLoginApplication.java
@@ -0,0 +1,13 @@
+package com.onlinerecharge.login.onlinerechargelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinerechargeLoginApplication {
+
+	public static void main(String[] args) {
+		System.out.println("This is for Test");
+		SpringApplication.run(OnlinerechargeLoginApplication.class, args);
+	}
+}
