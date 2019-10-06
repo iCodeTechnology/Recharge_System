5cfc458d055a1678d6eb6b63dd0631dcb1b7a1ed
Aniket Jadhav <aniketjadhav1213@gmail.com>
2019-10-06 17:58:33 +0530

    Welcome Page Added

--- rechargesystem/src/main/java/com/rechargesystem/controller/RechargeSystemController.java
+++ rechargesystem/src/main/java/com/rechargesystem/controller/RechargeSystemController.java
@@ -1,43 +1,77 @@
 package com.rechargesystem.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.rechargesystem.models.Login;
-import com.rechargesystem.service.LoginService;
-
 @Controller
 public class RechargeSystemController {
-	
+	/*
 	@Autowired
-	private LoginService loginService;
-	
+	private EmployeeService employeeService;
+	*/
 	@RequestMapping("/")
-	public String applicationLogin() {
-		System.out.println("AppController --> login");
-		
-		return "index";
+	public ModelAndView applicationLogin() {
+		ModelAndView mv= new ModelAndView("index");
+		return mv;	
 	}
+	
+	@RequestMapping("/about-us")
+	public ModelAndView aboutUs() {
+		ModelAndView mv= new ModelAndView("Web/about");
+		return mv;
+	}
+	
+	@RequestMapping("/contact-us")
+	public ModelAndView contactUs() {
+		ModelAndView mv= new ModelAndView("Web/contact");
+		return mv;
+	}
+	
 	@RequestMapping("/login")
-	public ModelAndView getLogin()
-	{
-		ModelAndView modelAndView = new ModelAndView();
-		List<Login> loginDetails = loginService.getLoginDetails();
-		System.out.println(loginDetails);
-		modelAndView.setViewName("index");
-		return modelAndView;
+	public ModelAndView Login() {
+		ModelAndView mv= new ModelAndView("Web/login");
+		return mv;
+	}
+	/*
+	@RequestMapping("/demo")
+	public ModelAndView demo() {
+		ModelAndView mv=new ModelAndView("demo");
+		mv.addObject("employee", employeeService.getAllEmployees());
+		System.out.println("Demo");
+		return mv;
 	}
 	
-	@RequestMapping("/admin")
-	public ModelAndView showLayout()
-	{
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("SuperAdmin/home");
-		return modelAndView;
+	@RequestMapping("/create-new")
+	public String createEmployee(Model model) {
+		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
+		return "create_employee";
 	}
 	
+	@RequestMapping(path = "/createEmployee", method = RequestMethod.POST)
+    public String saveEmployee(@ModelAttribute("Employee") Employee employee)
+    {
+		employeeService.saveEmployee(employee);
+        return "redirect:/demo";
+    }
+	
+	
+	@RequestMapping("/edit/{id}")
+    public ModelAndView editEmployee(@PathVariable(name = "id") Long id)
+    {
+        ModelAndView mav = new ModelAndView("edit_employee");
+        Employee employee = employeeService.getEmployee(id);
+        mav.addObject("employee", employee);
+        return mav;
+    }
+	
+	@RequestMapping("/delete/{id}")
+	public String deleteEmployee(@PathVariable(name = "id") Long id) {
+		
+		employeeService.deleteEmployee(id);
+		
+		return "redirect:/demo";
+	}*/
+
 }
